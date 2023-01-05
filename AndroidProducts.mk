Sohamlad7/android_device_motorola_cedric

cedric: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_cedric.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_cedric-user \
+    lineage_cedric-userdebug \
+    lineage_cedric-eng

cedric-experimental/android_device_motorola_cedric-twrp

AndroidProducts.mk
@@ -15,3 +15,6 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_cedric.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_cedric-eng